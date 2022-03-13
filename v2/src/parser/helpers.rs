d179b96d50b6a55e33b60945235e5b65abaed494
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2022-03-14 01:07:30 +0200

    Add token to op helper funtions

---
+++ v2/src/parser/helpers.rs
@@ -0,0 +1,28 @@
+use crate::ast::{BinaryOp, UnaryOp};
+use crate::tokens::Token;
+
+fn bin_op_from_token(t: &Token) -> BinaryOp {
+    match t {
+        &Token::Plus => BinaryOp::Plus,
+        &Token::Minus => BinaryOp::Minus,
+        &Token::Star => BinaryOp::Multiply,
+        &Token::Slash => BinaryOp::Divide,
+        &Token::Greater => BinaryOp::Greater,
+        &Token::GreaterEqual => BinaryOp::GreaterEqual,
+        &Token::Less => BinaryOp::Less,
+        &Token::LessEqual => BinaryOp::LessEqual,
+        &Token::EqualEqual => BinaryOp::IsEqual,
+        &Token::BangEqual => BinaryOp::IsNotEqual,
+        &Token::And => BinaryOp::And,
+        &Token::Or => BinaryOp::Or,
+        _ => panic!("Cant convert token {} to bin op", t)
+    }
+}
+
+fn unary_op_from_token(t: &Token) -> UnaryOp {
+    match t {
+        &Token::Minus => UnaryOp::Negate,
+        &Token::Not => UnaryOp::Not,
+        _ => panic!("Cant convert token {} to unary op", t)
+    }
+}
