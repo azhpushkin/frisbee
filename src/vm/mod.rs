838897860ab8e5a496138c81ffb0087eeca241b1
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2022-04-07 19:29:52 +0300

    Start implementing VM

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,30 +1,31 @@
-const STACK_SIZE = 256;
+const STACK_SIZE: usize = 256;
 
-
+// TODO: nan boxing?
 type Value = u64;
+
+struct CallFrame {
+    return_ip: usize,
+}
 
 struct Vm {
     program: Vec<u8>,
     stack: [Value; STACK_SIZE],
     stack_pointer: usize,
+    frames: Vec<CallFrame>, // TODO: limit size
 }
 
-
-
-
 enum Opcode {
-    Load,
-    Add,
-    Return
+    Load = 1,
+    Load_Int, // load int from -128 to 127
+    Add_i,
+    Add_f,
+    Call,
+    Return,
 }
 
 impl Vm {
     pub fn new(program: Vec<u8>) -> Self {
-        Vm {
-            program,
-            stack: [0; STACK_SIZE],
-            stack_pointer: 0,
-        }
+        Vm { program, stack: [0; STACK_SIZE], stack_pointer: 0, frames: vec![] }
     }
 
     fn push(&mut self, value: Value) {
@@ -37,16 +38,31 @@ impl Vm {
         self.stack[self.stack_pointer]
     }
 
+    pub fn read_next(&mut self) -> u8 {
+        let byte = self.program[self.stack_pointer];
+        self.stack_pointer += 1;
+        byte
+    }
+
     pub fn run(&mut self) {
         let mut pc = 0;
         while pc < self.program.len() {
-            let opcode = self.program[pc];
-            match opcode {
-                Load => {
+            match self.read_next() {
+                Load_int => {
                     let value = self.program[pc + 1];
                     self.stack[self.stack_pointer] = value;
                     self.stack_pointer += 1;
                     pc += 2;
+                }
+                Add_i | Add_f => {
+                    let a = self.pop();
+                    let b = self.pop();
+                    let result: Value = match opcode {
+                        Add_i => (a as i64) + (b as i64),
+                        Add_f => (a as f64) + (b as f64),
+                    } as Value;
+                    self.push(result);
+                    pc += 1;
                 }
             }
         }
