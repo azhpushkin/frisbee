f69a6afbb5337ce7f227636c46d384e24956d3b1
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2022-04-19 13:01:31 +0300

    Start refactoring disassembler

--- src/codegen/disassemble.rs
+++ src/codegen/disassemble.rs
@@ -43,92 +43,92 @@ pub fn opcode_to_s(c: u8) -> &'static str {
     }
 }
 
-pub fn get_str(i: &mut dyn Iterator<Item = (usize, &u8)>) -> String {
-    let n = u16::from_be_bytes(get_bytes::<2>(i));
-    let mut s = String::new();
-    for _ in 0..n {
-        s.push(*i.next().unwrap().1 as char);
-    }
-    s
-}
-pub fn get_bytes<const N: usize>(i: &mut dyn Iterator<Item = (usize, &u8)>) -> [u8; N] {
-    let mut bytes = [0; N];
-    for j in 0..N {
-        bytes[j] = *i.next().unwrap().1;
-    }
-    bytes
+pub struct Disassembler<'a> {
+    program: &'a Vec<u8>,
+    program_iter: Box<dyn Iterator<Item = (usize, &'a u8)> + 'a>,
 }
 
-pub fn disassemble_bytes(program: &Vec<u8>, aux: Option<&AuxData>) -> String {
-    let mut text_repr: String = String::from("Constants:\n");
-    let mut program_iter = program.iter().enumerate();
 
-    let mut i: usize = 0;
-    loop {
-        let const_text: String = match *program_iter.next().unwrap().1 {
-            op::CONST_INT_FLAG => i64::from_be_bytes(get_bytes::<8>(&mut program_iter)).to_string(),
-            op::CONST_FLOAT_FLAG => {
-                f64::from_be_bytes(get_bytes::<8>(&mut program_iter)).to_string()
+impl<'a> Disassembler<'a> {
+    pub fn new(program: &'a Vec<u8>) -> Self {
+        Disassembler {
+            program,
+            program_iter: Box::new(program.iter().enumerate()),
+        }
+    }
+
+    fn get_str(&mut self) -> String {
+        let n = u16::from_be_bytes(get_bytes::<2>());
+        let mut s = String::new();
+        for _ in 0..n {
+            s.push(*self.program_iter.next().unwrap().1 as char);
+        }
+        s
+    }
+
+    fn get_bytes<const N: usize>(&mut self) -> [u8; N] {
+        let mut bytes = [0; N];
+        for j in 0..N {
+            bytes[j] = *self.program_iter.next().unwrap().1;
+        }
+        bytes
+    }
+
+    fn disassemble_bytes(program: &Vec<u8>, aux: Option<&AuxData>) -> String {
+        let mut text_repr: String = String::from("Constants:\n");
+        let mut program_iter = program.iter().enumerate();
+    
+        let mut i: usize = 0;
+        loop {
+            let const_text: String = match *program_iter.next().unwrap().1 {
+                op::CONST_INT_FLAG => i64::from_be_bytes(get_bytes::<8>(&mut program_iter)).to_string(),
+                op::CONST_FLOAT_FLAG => {
+                    f64::from_be_bytes(get_bytes::<8>(&mut program_iter)).to_string()
+                }
+                op::CONST_STRING_FLAG => get_str(&mut program_iter),
+                op::CONST_END_FLAG => {
+                    let x = u16::from_be_bytes(get_bytes::<2>(&mut program_iter));
+                    text_repr.push_str(&format!("Entry: {:02x}", x));
+                    break;
+                }
+                c => panic!("Unknown const flag: {:02x}", c),
+            };
+            text_repr.push_str(&format!("\t{}: {}\n", i, const_text));
+            i += 1;
+        }
+        text_repr.push_str("\nFunctions:\n");
+    
+        while let Some((i, opcode)) = program_iter.next() {
+            if aux.is_some() && aux.unwrap().get(&i).is_some() {
+                let func = aux.unwrap().get(&i).unwrap();
+                text_repr.push_str(&format!("\n# {:?}:\n", func));
             }
-            op::CONST_STRING_FLAG => get_str(&mut program_iter),
-            op::CONST_END_FLAG => {
-                let x = u16::from_be_bytes(get_bytes::<2>(&mut program_iter));
-                text_repr.push_str(&format!("Entry: {:02x}", x));
-                break;
+    
+            let mut number_of_args = get_args_num(*opcode);
+            let mut args: Vec<u8> = vec![];
+            while number_of_args > 0 {
+                args.push(*program_iter.next().unwrap().1);
+                number_of_args -= 1;
             }
-            c => panic!("Unknown const flag: {:02x}", c),
-        };
-        text_repr.push_str(&format!("\t{}: {}\n", i, const_text));
-        i += 1;
+            let mut op_text = format!(" {:02x?}   {} {:02x?}", i, opcode_to_s(*opcode), args);
+    
+            // + 3 is added, because jump offset is relative to instruction pointer
+            // but `i` var points to jump opcode, which is 3 steps behind
+            // (1 step for jump itself and 2 for address of jump)
+            if *opcode == op::JUMP_IF_FALSE || *opcode == op::JUMP {
+                let x = u16::from_be_bytes([args[0], args[1]]) as usize;
+                op_text = format!("{} (jumps to {:02x?}) ", op_text, x + i + 3);
+            } else if *opcode == op::JUMP_BACK {
+                let x = u16::from_be_bytes([args[0], args[1]]) as usize;
+                op_text = format!("{} (jumps to {:02x?}) ", op_text, i - x + 3);
+            }
+    
+            text_repr.push_str(op_text.as_str());
+            text_repr.push_str("\n");
+        }
+        text_repr
     }
-    text_repr.push_str("\nFunctions:\n");
 
-    while let Some((i, opcode)) = program_iter.next() {
-        if aux.is_some() && aux.unwrap().get(&i).is_some() {
-            let func = aux.unwrap().get(&i).unwrap();
-            text_repr.push_str(&format!("\n# {:?}:\n", func));
-        }
 
-        let mut number_of_args = get_args_num(*opcode);
-        let mut args: Vec<u8> = vec![];
-        while number_of_args > 0 {
-            args.push(*program_iter.next().unwrap().1);
-            number_of_args -= 1;
-        }
-        let mut op_text = format!(" {:02x?}   {} {:02x?}", i, opcode_to_s(*opcode), args);
-
-        // + 3 is added, because jump offset is relative to instruction pointer
-        // but `i` var points to jump opcode, which is 3 steps behind
-        // (1 step for jump itself and 2 for address of jump)
-        if *opcode == op::JUMP_IF_FALSE || *opcode == op::JUMP {
-            let x = u16::from_be_bytes([args[0], args[1]]) as usize;
-            op_text = format!("{} (jumps to {:02x?}) ", op_text, x + i + 3);
-        } else if *opcode == op::JUMP_BACK {
-            let x = u16::from_be_bytes([args[0], args[1]]) as usize;
-            op_text = format!("{} (jumps to {:02x?}) ", op_text, i - x + 3);
-        }
-
-        text_repr.push_str(op_text.as_str());
-        text_repr.push_str("\n");
-    }
-    text_repr
 }
 
-type AuxData = HashMap<usize, SymbolFunc>;
-
-pub fn get_auxilary_functions_positions(prog: &ProgramAggregate) -> AuxData {
-    let (c, f) = generate_chunks(prog);
-
-    let mut functions_vec: Vec<SymbolFunc> = f.keys().map(|x| x.clone()).collect();
-    functions_vec.sort();
-
-    let mut functions_start: AuxData = HashMap::new();
-
-    let mut current_shift = c.len() + 2;
-    for name in functions_vec.iter() {
-        let bytecode = &f[name].bytecode;
-        functions_start.insert(current_shift, name.clone());
-        current_shift += bytecode.len();
-    }
-    functions_start
-}
