74a0e6234a017816a86edcdfa14ce33c520e3cbe
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2022-04-26 00:19:25 +0300

    Move statements codegen to impl

--- src/codegen/statements.rs
+++ src/codegen/statements.rs
@@ -1,5 +1,5 @@
 use crate::semantics::aggregate::RawFunction;
-use crate::semantics::light_ast::{LStatement, LExpr};
+use crate::semantics::light_ast::{LExpr, LStatement};
 use crate::semantics::symbols::SymbolType;
 use crate::vm::opcodes::op;
 
@@ -20,114 +20,115 @@ pub fn generate_function_bytecode(
     );
 
     for statement in func.body.iter() {
-        generate_statement_bytecode(statement, &mut generator, types_meta, None);
+        generator.push_statement(statement, None);
     }
 
     Ok(generator.get_bytecode())
 }
 
-fn generate_statement_bytecode<'a, 'b>(
-    statement: &'a LStatement,
-    generator: &mut BytecodeGenerator<'a, 'b>,
-    types_meta: &TypeMetadataTable,
-    loop_start: Option<usize>,
-) -> Vec<JumpPlaceholder> {
-    let mut break_placeholders = vec![];
-    match statement {
-        LStatement::Expression(expr) => {
-            generator.push_expr(expr);
-            generator.push_pop(&expr.expr_type);
-        }
-        LStatement::DeclareVar { var_type, name } => {
-            generator.add_local(name, var_type);
-            generator.push_reserve(var_type);
-        }
-        LStatement::AssignLocal { name, tuple_indexes, value } => {
-            generator.push_expr(value);
-            generator.push_set_local(name, tuple_indexes);
-        }
-        LStatement::DeclareAndAssignVar { var_type, name, value } => {
-            generator.add_local(name, var_type);
-            generator.push_expr(value);
-        }
-        LStatement::AssignToField { object, field, value } => {
-            let object_type: SymbolType = object.expr_type.into();
-            generator.push_expr(&object);
-            generator.push_expr(value);
-            generator.push(op::SET_TO_HEAP);
-            generator.push(types_meta.get(&object_type).field_offsets[field]);
-            generator.push(types_meta.get(&object_type).field_sizes[field]);
-        }
-        LStatement::Return(expr) => {
-            generator.push_expr(expr);
-            generator.push_set_return();
-            generator.push(op::RETURN);
-        }
-        LStatement::IfElse { condition, if_body, else_body } if else_body.is_empty() => {
-            generator.push_expr(condition);
-            generator.push(op::JUMP_IF_FALSE);
-
-            let placeholder_to_skip_ifbody: JumpPlaceholder = generator.push_placeholder();
-
-            for statement in if_body.iter() {
-                let br = generate_statement_bytecode(statement, generator, loop_start);
-                break_placeholders.extend(br);
+impl<'a, 'b> BytecodeGenerator<'a, 'b> {
+    pub fn push_statement(
+        &mut self,
+        statement: &'a LStatement,
+        loop_start: Option<usize>,
+    ) -> Vec<JumpPlaceholder> {
+        let mut outer_break_placeholders = vec![];
+        match statement {
+            LStatement::Expression(expr) => {
+                self.push_expr(expr);
+                self.push_pop(&expr.expr_type);
             }
-            generator.fill_placeholder(&placeholder_to_skip_ifbody);
-        }
-        LStatement::IfElse { condition, if_body, else_body } => {
-            generator.push_expr(condition);
-            generator.push(op::JUMP_IF_FALSE);
-
-            let placeholder_to_skip_ifbody = generator.push_placeholder();
-
-            for statement in if_body.iter() {
-                let br = generate_statement_bytecode(statement, generator, loop_start);
-                break_placeholders.extend(br);
+            LStatement::DeclareVar { var_type, name } => {
+                self.add_local(name, var_type);
+                self.push_reserve(var_type);
             }
-            generator.push(op::JUMP);
-            let placeholder_to_skip_elsebody = generator.push_placeholder();
-            generator.fill_placeholder(&placeholder_to_skip_ifbody);
-
-            for statement in else_body.iter() {
-                let br = generate_statement_bytecode(statement, generator, loop_start);
-                break_placeholders.extend(br);
+            LStatement::AssignLocal { name, tuple_indexes, value } => {
+                self.push_expr(value);
+                self.push_set_local(name, tuple_indexes);
             }
-            generator.fill_placeholder(&placeholder_to_skip_elsebody);
-        }
-        LStatement::While { condition, body } => {
-            let mut loop_breaks = vec![];
-            let start_pos = generator.get_position();
-            generator.push_expr(condition);
-
-            generator.push(op::JUMP_IF_FALSE);
-            let placeholder_to_skip_loop = generator.push_placeholder();
-
-            for statement in body.iter() {
-                let br = generate_statement_bytecode(statement, generator, Some(start_pos));
-                loop_breaks.extend(br);
+            LStatement::DeclareAndAssignVar { var_type, name, value } => {
+                self.add_local(name, var_type);
+                self.push_expr(value);
             }
-            generator.push(op::JUMP_BACK);
-            let placeholder_to_jump_back = generator.push_placeholder();
-
-            // jump back to condition
-            generator.fill_placeholder_backward(&placeholder_to_jump_back, start_pos);
-
-            // after loop is done - fill jumps related to breaks and condition failure
-            generator.fill_placeholder(&placeholder_to_skip_loop);
-            for break_placeholder in loop_breaks {
-                generator.fill_placeholder(&break_placeholder);
+            LStatement::AssignToField { object, field, value } => {
+                let object_type: SymbolType = object.expr_type.into();
+                self.push_expr(&object);
+                self.push_expr(value);
+                self.push(op::SET_TO_HEAP);
+                self.push(self.types_meta.get(&object_type).field_offsets[field]);
+                self.push(self.types_meta.get(&object_type).field_sizes[field]);
             }
-        }
-        LStatement::Break => {
-            generator.push(op::JUMP);
-            break_placeholders.push(generator.push_placeholder());
-        }
-        LStatement::Continue => {
-            generator.push(op::JUMP_BACK);
-            let placeholder_to_jump_back = generator.push_placeholder();
-            generator.fill_placeholder_backward(&placeholder_to_jump_back, loop_start.unwrap());
-        }
-    };
-    break_placeholders
+            LStatement::Return(expr) => {
+                self.push_expr(expr);
+                self.push_set_return();
+                self.push(op::RETURN);
+            }
+            LStatement::IfElse { condition, if_body, else_body } if else_body.is_empty() => {
+                self.push_expr(condition);
+                self.push(op::JUMP_IF_FALSE);
+
+                let placeholder_to_skip_ifbody: JumpPlaceholder = self.push_placeholder();
+
+                for statement in if_body.iter() {
+                    let breaks = self.push_statement(statement, loop_start);
+                    outer_break_placeholders.extend(breaks);
+                }
+                self.fill_placeholder(&placeholder_to_skip_ifbody);
+            }
+            LStatement::IfElse { condition, if_body, else_body } => {
+                self.push_expr(condition);
+                self.push(op::JUMP_IF_FALSE);
+
+                let placeholder_to_skip_ifbody = self.push_placeholder();
+
+                for statement in if_body.iter() {
+                    let breaks = self.push_statement(statement, loop_start);
+                    outer_break_placeholders.extend(breaks);
+                }
+                self.push(op::JUMP);
+                let placeholder_to_skip_elsebody = self.push_placeholder();
+                self.fill_placeholder(&placeholder_to_skip_ifbody);
+
+                for statement in else_body.iter() {
+                    let breaks = self.push_statement(statement, loop_start);
+                    outer_break_placeholders.extend(breaks);
+                }
+                self.fill_placeholder(&placeholder_to_skip_elsebody);
+            }
+            LStatement::While { condition, body } => {
+                let mut loop_breaks = vec![];
+                let start_pos = self.get_position();
+                self.push_expr(condition);
+
+                self.push(op::JUMP_IF_FALSE);
+                let placeholder_to_skip_loop = self.push_placeholder();
+
+                for statement in body.iter() {
+                    let breaks = self.push_statement(statement, Some(start_pos));
+                    loop_breaks.extend(breaks);
+                }
+                self.push(op::JUMP_BACK);
+                let placeholder_to_jump_back = self.push_placeholder();
+
+                // jump back to condition
+                self.fill_placeholder_backward(&placeholder_to_jump_back, start_pos);
+
+                // after loop is done - fill jumps related to breaks and condition failure
+                self.fill_placeholder(&placeholder_to_skip_loop);
+                for break_placeholder in loop_breaks {
+                    self.fill_placeholder(&break_placeholder);
+                }
+            }
+            LStatement::Break => {
+                self.push(op::JUMP);
+                outer_break_placeholders.push(self.push_placeholder());
+            }
+            LStatement::Continue => {
+                self.push(op::JUMP_BACK);
+                let placeholder_to_jump_back = self.push_placeholder();
+                self.fill_placeholder_backward(&placeholder_to_jump_back, loop_start.unwrap());
+            }
+        };
+        outer_break_placeholders
+    }
 }
