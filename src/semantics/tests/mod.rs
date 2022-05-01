9bf5238cab59d08dfcb6983d909a8cdafac0bd06
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2022-05-01 22:54:55 +0300

    Write test cases to check uninitialized

--- src/semantics/tests/mod.rs
+++ src/semantics/tests/mod.rs
@@ -1,3 +1,5 @@
-#[rustfmt::skip] #[cfg(test)] pub mod tests_symbols;
 #[rustfmt::skip] #[cfg(test)] pub mod helpers;
-#[rustfmt::skip] #[cfg(test)] pub mod tests_return_required;
+#[rustfmt::skip] #[cfg(test)] pub mod test_symbols;
+#[rustfmt::skip] #[cfg(test)] pub mod test_return_required;
+#[rustfmt::skip] #[cfg(test)] pub mod test_scopes;
+#[rustfmt::skip] #[cfg(test)] pub mod test_uninitialized;
---
+++ src/semantics/tests/test_uninitialized.rs
@@ -0,0 +1,121 @@
+use super::helpers::{assert_semantic_check_fails, assert_semantic_check_is_fine};
+
+assert_semantic_check_fails!(
+    simple_case_of_uninitialized,
+    r#"
+    ===== file: main.frisbee
+    fun void main() {
+        Int b;
+        b = b + 1;  // ERR: Variable `b` might be uninitialized here
+    }
+    "#
+);
+
+assert_semantic_check_fails!(
+    uninitialized_after_branching,
+    r#"
+    ===== file: main.frisbee
+    fun void main() {
+        Int a;
+        if true { a = 1; }
+        else { }  // nothing here, so assume a can be uninitialized
+        a + 1;  // ERR: Variable `a` might be uninitialized here
+    }
+    "#
+);
+
+
+assert_semantic_check_fails!(
+    uninitialized_after_branching_with_elif,
+    r#"
+    ===== file: main.frisbee
+    fun void main() {
+        Int a;
+        if true { a = 1; }
+        elif true { a = 2; }
+        else { }  // nothing here, so assume a can be uninitialized
+        } else {
+            
+        }
+        a + 1;  // ERR: Variable `a` might be uninitialized here
+    }
+    "#
+);
+
+
+assert_semantic_check_is_fine!(
+    initialized_after_single_if,
+    r#"
+    ===== file: main.frisbee
+    fun void main() {
+        Int a;
+        if false { a = 1; }
+        a + 1;  // ERR: Variable `a` might be uninitialized here
+    }
+    "#
+);
+
+assert_semantic_check_is_fine!(
+    initialized_after_single_if_elif_else,
+    r#"
+    ===== file: main.frisbee
+    fun void main() {
+        Int a;
+        if false { a = 1; }
+        elif false {a = 2; }
+        else { a = 3; }
+        a + 1;
+    }
+    "#
+);
+
+assert_semantic_check_fails!(
+    still_unitialized_after_while,
+    r#"
+    ===== file: main.frisbee
+    fun void main() {
+        Int a;
+        while false { a = 1; a + 1;}
+        a + 2;  // ERR: Variable `a` might be uninitialized here
+    }
+    "#
+);
+
+assert_semantic_check_fails!(
+    uninitialized_if_continue_occures,
+    r#"
+    ===== file: main.frisbee
+    fun void main() {
+        Int a;
+        while false {
+            if true {
+                continue;
+                a = 1;
+            } 
+            a + 3;  // ERR: Variable `a` might be uninitialized here
+        }
+    }
+    "#
+);
+
+assert_semantic_check_fails!(
+    uninitialized_if_break_occures,
+    r#"
+    ===== file: main.frisbee
+    fun void main() {
+        Int a;
+        while true {
+            if true {
+                a = 1;
+            } else {
+                // so from the code it is obvious that `a` is always set
+                // but the fact that there is possible break must make
+                // it impossible to know if `a` is initialized or not
+                if false { break; }
+                a = 2;
+            }
+            a + 3;  // ERR: Variable `a` might be uninitialized here
+        }
+    }
+    "#
+);
