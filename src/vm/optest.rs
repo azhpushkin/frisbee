21c6a5700f2bc33ebb51370e22458335e17a38bf
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2022-05-12 14:43:35 +0300

    Store arguments num in macro accumulator

--- src/vm/optest.rs
+++ src/vm/optest.rs
@@ -13,29 +13,44 @@ const LOAD_VALUE: u8 = 1;
 */
 
 macro_rules! opcodes_list {
-    ($s:expr, $c:ident($args:literal)  ) => {
-        pub const $c: u8 = $s;
-    };
-    ($s:expr, $c:ident($args:literal), $ ( $more:ident($margs:literal) ),+ $(,)? ) => {
+    ($s:expr, [$($acc:expr),*], $c:ident($args:literal)  ) => {
         pub const $c: u8 = $s;
 
-        opcodes_list!($s+1, $(  $more($margs) ),+ );
+        const ARGS_NUM: [u8; $s+1] = [$($acc),*, $args];
+    };
+    ($s:expr, [$($acc:expr),+], $c:ident($args:literal), $ ( $more:ident($margs:literal) ),+ $(,)? ) => {
+        pub const $c: usize = $s;
+
+        opcodes_list!($s+1, [$($acc),*, $args], $(  $more($margs) ),+ );
+    };
+
+    // Empty array call
+    ($s:expr, [], $c:ident($args:literal), $ ( $more:ident($margs:literal) ),+ $(,)? ) => {
+        pub const $c: u8 = $s;
+
+        opcodes_list!($s+1, [$args], $(  $more($margs) ),+ );
+    };
+    
+    // Initial call
+    ($c:ident($args:literal), $ ( $more:ident($margs:literal) ),+ $(,)? ) => {
+        opcodes_list!(0, [], $c($args), $(  $more($margs) ),+ );
     };
 }
 
 
 mod op {
-    macro_rules! args_num {
-        (op::$s:ident) => {
-            $s::ARGS
-        };
+    // macro_rules! args_num {
+    //     (op::$s:ident) => {
+    //         $s::ARGS
+    //     };
+    // }
+
+    // pub(crate) use args_num;
+    pub fn get_args_num(opcode: u8) -> usize {
+        ARGS_NUM[opcode as usize] as usize
     }
 
-    pub(crate) use args_num;
-
-
     opcodes_list!(
-        0,
         LOAD_VALUE(3),
         LOAD_ANOTHER(12),
         X(2),
@@ -52,6 +67,14 @@ mod test {
         assert_eq!(op::LOAD_ANOTHER, 1);
         assert_eq!(op::X, 2);
         assert_eq!(op::Y, 3);
+    }
+
+    #[test]
+    fn test_args_num() {
+        assert_eq!(op::get_args_num[op::LOAD_VALUE as usize], 3);
+        assert_eq!(op::get_args_num[op::LOAD_ANOTHER as usize], 12);
+        assert_eq!(op::get_args_num[op::X as usize], 2);
+        assert_eq!(op::get_args_num[op::Y as usize], 0);
     }
 }
 
